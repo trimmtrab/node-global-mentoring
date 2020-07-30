38c3d7dfe2441414d6e959d12909b7d35620d09f
Artem <atm.zmn95@gmail.com>
2020-07-30 20:50:09 +0300

    Home task1 Review Fixes

--- src/task2.js
+++ src/task2.js
@@ -6,10 +6,15 @@ const CSV_FILE_PATH = "./csv/nodejs-hw1-ex1.csv";
 const RES_FILE_PATH = "./task2.result.txt";
 
 const readStream = fs.createReadStream(CSV_FILE_PATH);
+const csvToJsonStream = csv();
 const writeStream = fs.createWriteStream(RES_FILE_PATH, { flags: "w+" });
 
-stream.pipeline(readStream, csv(), writeStream, (err) => {
-  if (err) {
-    console.error(err);
+stream.pipeline(
+  readStream,
+  csvToJsonStream,
+  writeStream, (err) => {
+    if (err) {
+      console.error(err);
+    }
   }
-});
+);
