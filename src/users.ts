906791bca87df4d458269ae930164a17a10294c8
Artem <atm.zmn95@gmail.com>
2020-08-17 22:03:41 +0300

    Home Task 2

---
+++ src/users.ts
@@ -0,0 +1,25 @@
+const mockUsers = require('./mock');
+
+type User = {
+  id: string;
+  login: string;
+  password: string;
+  age: number;
+  isDeleted: boolean;
+}
+
+const createUserToSend = (user: User) => ({
+  id: user.id,
+  login: user.login,
+  age: user.age
+});
+
+const users: User[] = mockUsers;
+
+const findUser = (id: User['id']) => users.find(user => !user.isDeleted && user.id === id);
+
+module.exports = {
+  createUserToSend,
+  findUser,
+  users
+};
---
+++ src/validation.ts
@@ -0,0 +1,34 @@
+import Joi = require('joi');
+import { RequestHandler } from 'express';
+
+const schema = Joi.object({
+    age: Joi.number().min(4).max(130).required(),
+    login: Joi.string().required(),
+    password: Joi.string().alphanum().pattern(/[A-Za-z]/).pattern(/\d/).required()
+});
+
+const errorResponse = (schemaErrors: Joi.ValidationErrorItem[]) => {
+    const errors = schemaErrors.map(({ path, message }) => ({
+        path,
+        message
+    }));
+
+    return {
+        status: 'failed',
+        errors
+    };
+};
+
+const validateUser: RequestHandler = (req, res, next) => {
+    const { error } = schema.validate(req.body, {
+        abortEarly: false
+    });
+
+    if (error && error.isJoi) {
+        res.status(400).json(errorResponse(error.details));
+    } else {
+        next();
+    }
+};
+
+module.exports = validateUser;
