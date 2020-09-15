d94489141ed0253f04336b221dac84b0b8912d55
Artem <atm.zmn95@gmail.com>
2020-09-15 21:36:23 +0300

    Home Task 5

--- src/routers/groups.ts
+++ src/routers/groups.ts
@@ -1,67 +1,18 @@
 import express = require('express');
-import { GroupService } from '../services/groups';
+import { catchAsync } from '../errorHandlers';
 import { validateGroupCreationParams, validateGroupUpdateParams } from '../validation/groups';
+import { GroupController } from '../controllers/groups';
 
 export const groupRouter = express.Router();
 
-groupRouter.put('/addUsers', async (req, res) => {
-    const { groupId, userIds } = req.body;
-    const areUsersAdded = await GroupService.addUsersToGroup(groupId, userIds);
-
-    if (!areUsersAdded) {
-        res.sendStatus(400);
-    }
-    res.end();
-});
+groupRouter.put('/addUsers',
+    catchAsync(GroupController.addUsers));
 
 groupRouter.route('/:id')
-    .delete(async (req, res) => {
-        const { id } = req.params;
-        const isGroupDeleted = await GroupService.delete(id);
-
-        if (!isGroupDeleted) {
-            res.sendStatus(404);
-        }
-
-        res.end();
-    })
-    .get(async (req, res) => {
-        const { id } = req.params;
-        const group = await GroupService.get(id);
-
-        if (!group) {
-            res.sendStatus(404);
-        }
-
-        res.send(group).end();
-    })
-    .put(validateGroupUpdateParams, async (req, res) => {
-        const { id } = req.params;
-        const group = await GroupService.update(id, req.body);
-
-        if (!group) {
-            res.sendStatus(404);
-        }
-
-        res.send(group).end();
-    });
+    .delete(catchAsync(GroupController.delete))
+    .get(catchAsync(GroupController.get))
+    .put(validateGroupUpdateParams, catchAsync(GroupController.put));
 
 groupRouter.route('/')
-    .get(async (req, res) => {
-        const groups = await GroupService.getAll();
-
-        if (!groups) {
-            res.sendStatus(400);
-        }
-
-        res.send(groups).end();
-    })
-    .post(validateGroupCreationParams, async (req, res) => {
-        const newGroupId = await GroupService.create(req.body);
-
-        if (!newGroupId) {
-            res.sendStatus(400);
-        }
-
-        res.status(201).send(newGroupId).end();
-    });
+    .get(catchAsync(GroupController.getAll))
+    .post(validateGroupCreationParams, catchAsync(GroupController.post));
